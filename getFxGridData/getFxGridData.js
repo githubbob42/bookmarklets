06201e2ddbe5417614e85369b67d0e17cec0a7f5
Bob Roth <broth@liquidframeworks.com>
2016-03-11 13:08:26 -0600

    add header test to json

--- getFxGridData/getFxGridData.js
+++ getFxGridData/getFxGridData.js
@@ -1,17 +1,26 @@
-javascript:(function() { 
+javascript:(function() {
     var tbody = $("tbody");
     if (!tbody.length) return;
+    var thead = $("thead");
 
     var json = {};
+    var headers = json[$(tbody[0]).attr("data-id") + "_headers"] = [];
     var data = json[$(tbody[0]).attr("data-id")] = [];
 
-    for (var rowNum=0; rowNum < tbody[0].rows.length; rowNum++) { 
+    for (var rowNum=0; rowNum < thead[0].rows.length; rowNum++) {
+        for (var cellNum=0; cellNum < thead[0].rows[rowNum].cells.length; cellNum++ ) {
+            var cell = $(thead[0].rows[rowNum].cells[cellNum]);
+            headers.push(cell.text().trim());
+        }
+    }
+
+    for (var rowNum=0; rowNum < tbody[0].rows.length; rowNum++) {
         var rowData = {};
-        for (var cellNum=0; cellNum < tbody[0].rows[rowNum].cells.length; cellNum++ ) { 
+        for (var cellNum=0; cellNum < tbody[0].rows[rowNum].cells.length; cellNum++ ) {
             var cell = $(tbody[0].rows[rowNum].cells[cellNum]);
             if (!cell.attr("data-id")) continue;
             rowData[cell.attr("data-id").replace("field-", "")] = cell.text().trim();
-        } 
+        }
         data.push(rowData);
     }
 
