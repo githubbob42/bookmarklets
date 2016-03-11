197ada0ab9e63ca01d89c0bf8a873c5952dea683
Bob Roth <broth@liquidframeworks.com>
2016-03-11 15:28:08 -0600

    include header labels in json

--- getFxGridData/getFxGridData.js
+++ getFxGridData/getFxGridData.js
@@ -1,27 +1,32 @@
 javascript:(function() {
     var tbody = $("tbody");
     if (!tbody.length) return;
-    var thead = $("thead");
 
     var json = {};
-    var headers = json[$(tbody[0]).attr("data-id") + "_headers"] = [];
-    var data = json[$(tbody[0]).attr("data-id")] = [];
+    var headers, data;
 
-    for (var rowNum=0; rowNum < thead[0].rows.length; rowNum++) {
-        for (var cellNum=0; cellNum < thead[0].rows[rowNum].cells.length; cellNum++ ) {
-            var cell = $(thead[0].rows[rowNum].cells[cellNum]);
-            headers.push(cell.text().trim());
+    var thead = $("thead");
+    for (var hdrNum=0; hdrNum < thead.length; hdrNum++) {
+        headers = json[$(tbody[hdrNum]).attr("data-id") + "_headers"] = [];
+        for (var rowNum=0; rowNum < thead[hdrNum].rows.length; rowNum++) {
+            for (var cellNum=0; cellNum < thead[0].rows[rowNum].cells.length; cellNum++ ) {
+                var cell = $(thead[0].rows[rowNum].cells[cellNum]);
+                headers.push(cell.text().trim());
+            }
         }
     }
 
-    for (var rowNum=0; rowNum < tbody[0].rows.length; rowNum++) {
-        var rowData = {};
-        for (var cellNum=0; cellNum < tbody[0].rows[rowNum].cells.length; cellNum++ ) {
-            var cell = $(tbody[0].rows[rowNum].cells[cellNum]);
-            if (!cell.attr("data-id")) continue;
-            rowData[cell.attr("data-id").replace("field-", "")] = cell.text().trim();
+    for (var bodyNum=0; bodyNum < tbody.length; bodyNum++) {
+        data = json[$(tbody[bodyNum]).attr("data-id")] = [];
+        for (var rowNum=0; rowNum < tbody[bodyNum].rows.length; rowNum++) {
+            var rowData = {};
+            for (var cellNum=0; cellNum < tbody[bodyNum].rows[rowNum].cells.length; cellNum++ ) {
+                var cell = $(tbody[bodyNum].rows[rowNum].cells[cellNum]);
+                if (!cell.attr("data-id")) continue;
+                rowData[cell.attr("data-id").replace("field-", "")] = cell.text().trim();
+            }
+            data.push(rowData);
         }
-        data.push(rowData);
     }
 
     var page = '<html><head></head><body><pre style=&quot;padding:5px 10px;font-family:Courier New,Courier;font-size:11pt;text-align:left&quot;>' + JSON.stringify(json, null, 2) + '</pre></body></html>';
