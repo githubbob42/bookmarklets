b7d4bb23d74e9ecd40326d96e3cb298c8a83f08b
Bob Roth <broth@liquidframeworks.com>
2016-03-11 11:17:24 -0600

    add getFxGridData bookmarklet

---
+++ getFxGridData/getFxGridData.js
@@ -0,0 +1,21 @@
+javascript:(function() { 
+    var tbody = $("tbody");
+    if (!tbody.length) return;
+
+    var json = {};
+    var data = json[$(tbody[0]).attr("data-id")] = [];
+
+    for (var rowNum=0; rowNum < tbody[0].rows.length; rowNum++) { 
+        var rowData = {};
+        for (var cellNum=0; cellNum < tbody[0].rows[rowNum].cells.length; cellNum++ ) { 
+            var cell = $(tbody[0].rows[rowNum].cells[cellNum]);
+            if (!cell.attr("data-id")) continue;
+            rowData[cell.attr("data-id").replace("field-", "")] = cell.text().trim();
+        } 
+        data.push(rowData);
+    }
+
+    var page = '<html><head></head><body><pre style=&quot;padding:5px 10px;font-family:Courier New,Courier;font-size:11pt;text-align:left&quot;>' + JSON.stringify(json, null, 2) + '</pre></body></html>';
+    var w = window.open();
+    $(w.document.body).html(page);
+})()
